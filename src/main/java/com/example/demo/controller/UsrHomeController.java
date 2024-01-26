c0f72e453ecf9cf257fc9c8984ad8105cc820639
pshpark7 <pshpark07@gmail.com>
2024-01-26 17:18:03 +0900

    count 초기화

--- src/main/java/com/example/demo/controller/UsrHomeController.java
+++ src/main/java/com/example/demo/controller/UsrHomeController.java
@@ -17,6 +17,13 @@ public class UsrHomeController {
 	public UsrHomeController() {
 		count = 0;
 	}
+	@RequestMapping("/usr/home/main5")
+	@ResponseBody
+	public String showMain5() {
+		count = 0;
+		return "count의 값이 0으로 초기화 되었습니다.";
+	}
+	
 	@RequestMapping("/usr/home/main")
 	@ResponseBody
 	public String showMain() {
