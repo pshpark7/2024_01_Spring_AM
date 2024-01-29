f2e603244520ae34784f995ce7fe08ad2b211c31
pshpark7 <pshpark07@gmail.com>
2024-01-29 13:22:25 +0900

    게시물 수정 기능 구현, 상세보기 구현

--- src/main/java/com/example/demo/controller/UsrArticleController.java
+++ src/main/java/com/example/demo/controller/UsrArticleController.java
@@ -14,6 +14,7 @@ public class UsrArticleController {
 	int lastArticleId;
 	List<Article> articles;
 
+	// 생성자
 	public UsrArticleController() {
 		lastArticleId = 0;
 		articles = new ArrayList<>();
@@ -21,6 +22,7 @@ public class UsrArticleController {
 		makeTestData();
 	}
 
+	// 서비스 메서드
 	private void makeTestData() {
 		for (int i = 1; i <= 10; i++) {
 			String title = "제목" + i;
@@ -39,6 +41,71 @@ public class UsrArticleController {
 		return article;
 	}
 
+	private void deleteArticle(int id) {
+		Article article = getArticle(id);
+		articles.remove(article);
+	}
+
+	private void modifyArticle(int id, String title, String body) {
+		Article article = getArticle(id);
+
+		article.setTitle(title);
+		article.setBody(body);
+	}
+
+	private Article getArticle(int id) {
+		for (Article article : articles) {
+			if (article.getId() == id) {
+				return article;
+			}
+		}
+		return null;
+	}
+
+	// 액션 메서드
+
+	@RequestMapping("/usr/article/getArticle")
+	@ResponseBody
+	public Object getArticleAction(int id) {
+		Article article = getArticle(id);
+
+		if (article == null) {
+			return id + "번 글은 존재하지 않습니다.";
+		}
+
+		return article;
+	}
+
+	@RequestMapping("/usr/article/doModify")
+	@ResponseBody
+	public Object doModify(int id, String title, String body) {
+
+		Article article = getArticle(id);
+
+		if (article == null) {
+			return id + "번 글은 존재하지 않습니다.";
+		}
+
+		modifyArticle(id, title, body);
+
+		return article;
+	}
+
+	@RequestMapping("/usr/article/doDelete")
+	@ResponseBody
+	public String doDelete(int id) {
+
+		Article article = getArticle(id);
+
+		if (article == null) {
+			return id + "번 글은 존재하지 않습니다.";
+		}
+
+		deleteArticle(id); 
+
+		return id + "번 글이 삭제 되었습니다";
+	}
+
 	@RequestMapping("/usr/article/doAdd")
 	@ResponseBody
 	public Article doAdd(String title, String body) {
@@ -51,43 +118,4 @@ public class UsrArticleController {
 	public List<Article> getArticles() {
 		return articles;
 	}
-
-	@RequestMapping("/usr/article/doDelete")
-	@ResponseBody
-	public String doDelete(int id) {
-		Article article = getArticle(id);
-		if (article == null) {
-			return String.format("%d번 글은 없습니다.", id);
-		}
-		deleteArticle(id);
-		return String.format("%d번 글이 삭제되었습니다", id);
-	}
-
-	private void deleteArticle(int id) {
-		Article article = getArticle(id);
-		articles.remove(article);
-	}
-
-	private Article getArticle(int id) {
-		for (Article article : articles) {
-			if(article.getId() == id) {
-				return article;
-			}
-		}
-		return null;
-	}
-
-	@RequestMapping("/usr/article/doModify")
-	@ResponseBody
-	public Object doModify(int id, String title, String body) {
-		Article foundarticle = articles.get(id - 1);
-		if (foundarticle == null) {
-			return String.format("%d번 글은 없습니다.", id);
-		}
-		foundarticle.setTitle(title);
-		foundarticle.setTitle(body);
-		return foundarticle + String.format("%d번 글이 수정되었습니다", id);
-	}
-
-
 }
