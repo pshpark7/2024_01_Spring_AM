4548abd064fa7edce122ec3f9b4cbd300b3ff86e
pshpark7 <pshpark07@gmail.com>
2024-01-29 11:18:00 +0900

    게시물 테스트 데이터 생성, 작성 로직 중복 제거, 수정/삭제 구현중

--- src/main/java/com/example/demo/controller/UsrArticleController.java
+++ src/main/java/com/example/demo/controller/UsrArticleController.java
@@ -9,33 +9,71 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.demo.vo.Article;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-
 @Controller
 public class UsrArticleController {
+	int lastArticleId;
+	List<Article> articles;
 
-	int lastId = 0;
-	List<Article> articles = new ArrayList<>();
+	public UsrArticleController() {
+		lastArticleId = 0;
+		articles = new ArrayList<>();
 
+		makeTestData();
+	}
+
+	private void makeTestData() {
+		for (int i = 1; i <= 10; i++) {
+			String title = "제목" + i;
+			String body = "내용" + i;
+
+			writeArticle(title, body);
+		}
+	}
+
+	public Article writeArticle(String title, String body) {
+		int id = lastArticleId + 1;
+		Article article = new Article(id, title, body);
+		articles.add(article);
+		lastArticleId++;
+
+		return article;
+	}
 
 	@RequestMapping("/usr/article/doAdd")
 	@ResponseBody
 	public Article doAdd(String title, String body) {
-		int id = lastId + 1;
-		Article article = new Article(id, title, body);
-		articles.add(article);
-		lastId++;
+		Article article = writeArticle(title, body);
 		return article;
 	}
 
 	@RequestMapping("/usr/article/getArticles")
 	@ResponseBody
 	public List<Article> getArticles() {
-
 		return articles;
-}
+	}
+
+	@RequestMapping("/usr/article/doDelete")
+	@ResponseBody
+	public String doDelete(int id) {
+		Article foundarticle = articles.get(id - 1);
+		if (foundarticle == null) {
+			return String.format("%d번 글은 없습니다.", id);
+		}
+		articles.remove(id - 1);
+		return String.format("%d번 글이 삭제되었습니다", id);
+	}
+
+	@RequestMapping("/usr/article/doModify")
+	@ResponseBody
+	public Object doModify(int id, String title, String body) {
+		Article foundarticle = articles.get(id - 1);
+		if (foundarticle == null) {
+			return String.format("%d번 글은 없습니다.", id);
+		}
+		foundarticle.setTitle(title);
+		foundarticle.setTitle(body);
+		return foundarticle + String.format("%d번 글이 수정되었습니다", id);
+	}
+
 
 }
