1d1c2b82c2c14bda8b85e4c08883103beca6a433
pshpark7 <pshpark07@gmail.com>
2024-01-29 09:11:08 +0900

    여러가지 리턴타입 실험

---
+++ src/main/java/com/example/demo/controller/Article.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Article {
+	public Article(int i, String string) {
+		int id;
+		String name;
+
+	}
+}
--- src/main/java/com/example/demo/controller/UsrHomeController.java
+++ src/main/java/com/example/demo/controller/UsrHomeController.java
@@ -1,34 +1,71 @@
 package com.example.demo.controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import jakarta.annotation.Generated;
-
 
 @Controller
 public class UsrHomeController {
 	
-	private int count;
-	
-	public UsrHomeController() {
-		count = 0;
-	}
-	@RequestMapping("/usr/home/setCount")
+	@RequestMapping("/usr/home/getList")
 	@ResponseBody
-	public String setCount(int count) {
-		this.count = count;
-		return "count의 값이 " + count + "으로 초기화 되었습니다.";
+	public List<String> getList() {
+		List<String> list = new ArrayList<>();
+		list.add("야구");
+		list.add("축구");
+		return list;
+
+	}
+	@RequestMapping("/usr/home/getMap")
+	@ResponseBody
+	public Map<String, Object> getMap() {
+		Map<String, Object> map = new HashMap<>();
+		map.put("야구", "baseball");
+		map.put("축구", "soccer");
+		
+		return map;
+
 	}
 
-	@RequestMapping("/usr/home/getCount")
+	@RequestMapping("/usr/home/getInt")
 	@ResponseBody
-	
-	public int getCount() {
-		return count++;
-		
+	public int getint() {
+		return 1;
+
 	}
+
+	@RequestMapping("/usr/home/getString")
+	@ResponseBody
+	public String getString() {
+		return "abc";
+
+	}
+	@RequestMapping("/usr/home/getBoolean")
+	@ResponseBody
+	public boolean getBoolean() {
+		return true;
+
+	}
+	@RequestMapping("/usr/home/getDouble")
+	@ResponseBody
+	public double getDouble() {
+		return 3.3;
+
+	}
+	
+
+	@RequestMapping("/usr/home/getArticle")
+	@ResponseBody
+	public Article getArticle() {
+		Article article = new Article(1, "제목1");
+		
+		return article;
+
+	}
+
 }
