2e2627f2dca4f2652de21b5702010016063a54d7
pshpark7 <pshpark07@gmail.com>
2024-01-30 15:18:21 +0900

    Ut.f, 문장 구성을 쉽게 할 수 있도록

--- src/main/java/com/example/demo/util/Ut.java
+++ src/main/java/com/example/demo/util/Ut.java
@@ -5,27 +5,32 @@ import java.util.Map;
 
 public class Ut {
 
+	public static Object f(String format, Object... args) {
+		return String.format(format, args);
+	}
+
 	public static boolean isNullOrEmpty(String str) {
 		return str == null || str.trim().length() == 0;
 	}
-	
+
 	public static boolean isEmpty(Object obj) {
 		if (obj == null) {
 			return true;
 		}
+
 		if (obj instanceof String) {
 			return ((String) obj).trim().length() == 0;
 		}
-		
+
 		if (obj instanceof Map) {
 			return ((Map<?, ?>) obj).isEmpty();
 		}
-		
+
 		if (obj.getClass().isArray()) {
 			return Array.getLength(obj) == 0;
 		}
+
 		return false;
-		
 	}
 
 }
